fb63d25cce6fdac8e5a9e46a547e2dd04024affd
PySuger <zhanzizhi@gmail.com>
2024-10-18 03:30:31 +0800

    add websocket heartbeat

--- examples/net/heartbeat/websocket/client/main.go
+++ examples/net/heartbeat/websocket/client/main.go
@@ -17,19 +17,46 @@ func main() {
 	defer conn.Close()
 	log.Println("Connected to server")
 
-	for {
-		if er := conn.WriteMessage(websocket.TextMessage, []byte("Hello, WebSocket!")); er != nil {
-			log.Printf("Write error: %v", er)
-			break
-		}
+	conn.SetPongHandler(func(appData string) error {
+		log.Printf("Received Pong from server: %s\n", appData)
+		return nil
+	})
 
-		_, message, er := conn.ReadMessage()
+	conn.SetPingHandler(func(appData string) error {
+		log.Printf("Received Ping from server: %s\n", appData)
+		if er := conn.WriteControl(websocket.PongMessage, []byte(appData), time.Now().Add(5*time.Second)); er != nil {
+			log.Printf("Failed to send Pong: %v", er)
+			return er
+		}
+		log.Println("Pong sent to server")
+		return nil
+	})
+
+	go startHeartbeat(conn)
+
+	for {
+		messageType, message, er := conn.ReadMessage()
 		if er != nil {
 			log.Printf("Read error: %v", er)
 			break
 		}
-		log.Printf("Received: %s", message)
+		log.Printf("Received message: %s:%d\n", message, messageType)
+	}
+}
 
-		time.Sleep(5 * time.Second)
+func startHeartbeat(conn *websocket.Conn) {
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			err := conn.WriteControl(websocket.PingMessage, []byte("ping"), time.Now().Add(5*time.Second))
+			if err != nil {
+				log.Printf("Failed to send Ping: %v", err)
+				return
+			}
+			log.Println("Ping sent to server")
+		}
 	}
 }
--- examples/net/heartbeat/websocket/server/main.go
+++ examples/net/heartbeat/websocket/server/main.go
@@ -3,43 +3,70 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
-		return true
+		return true // 允许所有来源
 	},
 }
 
 func main() {
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Printf("Upgrade error: %v", err)
-			return
-		}
-		defer conn.Close()
-		log.Println("Client connected")
+	http.HandleFunc("/ws", handleWebSocket)
 
-		for {
-			messageType, message, er := conn.ReadMessage()
-			if er != nil {
-				log.Printf("Read error: %v", er)
-				break
-			}
-			log.Printf("Received: %s", message)
-
-			if er := conn.WriteMessage(messageType, message); er != nil {
-				log.Printf("Write error: %v", er)
-				break
-			}
-		}
-	})
-
-	log.Println("Server is listening on :8080")
+	log.Println("WebSocket server is listening on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatalf("ListenAndServe error: %v", err)
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("Upgrade error: %v", err)
+		return
+	}
+	defer conn.Close()
+	log.Println("Client connected")
+
+	conn.SetPongHandler(func(appData string) error {
+		log.Printf("Received Pong from client: %s\n", appData)
+		return nil
+	})
+	go startHeartbeat(conn)
+
+	for {
+		messageType, message, er := conn.ReadMessage()
+
+		if er != nil {
+			log.Printf("Read error: %v", er)
+			break
+		}
+		log.Printf("Received: %s, type: %d", message, messageType)
+
+		if er := conn.WriteMessage(messageType, message); er != nil {
+			log.Printf("Write error: %v", er)
+			break
+		}
+	}
+}
+
+func startHeartbeat(conn *websocket.Conn) {
+	ticker := time.NewTicker(15 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			err := conn.WriteControl(websocket.PingMessage, []byte("ping"), time.Now().Add(5*time.Second))
+			if err != nil {
+				log.Printf("Failed to send Ping: %v", err)
+				return
+			}
+			log.Println("Ping sent to client")
+		}
 	}
 }
