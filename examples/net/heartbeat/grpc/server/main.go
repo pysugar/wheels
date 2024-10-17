9048b95ecd96dcdc680a0c27490063f695c4b091
PySuger <zhanzizhi@gmail.com>
2024-10-18 03:19:16 +0800

    grpc bind channelz health reflection

--- examples/net/heartbeat/grpc/server/main.go
+++ examples/net/heartbeat/grpc/server/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/channelz/service"
 	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
 	"os"
+	"time"
 
 	pb "github.com/pysugar/wheels/examples/net/heartbeat/grpc/heartbeat"
 )
@@ -24,12 +30,24 @@ func main() {
 	logger := grpclog.NewLoggerV2(os.Stdout, os.Stdout, os.Stderr)
 	grpclog.SetLoggerV2(logger)
 
+	kaParams := keepalive.ServerParameters{
+		MaxConnectionIdle:     5 * time.Minute,
+		MaxConnectionAge:      2 * time.Hour,
+		MaxConnectionAgeGrace: 5 * time.Minute,
+		Time:                  1 * time.Hour,
+		Timeout:               20 * time.Second,
+	}
+
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
+	s := grpc.NewServer(grpc.KeepaliveParams(kaParams))
+
+	grpc_health_v1.RegisterHealthServer(s, health.NewServer())
+	reflection.Register(s)
+	service.RegisterChannelzServiceToServer(s)
 	pb.RegisterHeartbeatServiceServer(s, &server{})
 
 	log.Printf("Server is listening on port %d", 50051)
