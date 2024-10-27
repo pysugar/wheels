48530ebb002d74cf03f8e83b9dd3275bb0d6b6f0
PySuger <zhanzizhi@gmail.com>
2024-10-27 17:40:48 +0800

    fix bugs

--- cmd/subcmds/fetch.go
+++ cmd/subcmds/fetch.go
@@ -7,16 +7,22 @@ import (
 	"encoding/base64"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"net/url"
 	"strings"
+	"sync/atomic"
 	"time"
 
 	"github.com/pysugar/wheels/binproto/http2"
 	"github.com/pysugar/wheels/cmd/base"
 	"github.com/spf13/cobra"
 	"golang.org/x/net/http2/hpack"
+)
+
+var (
+	streamID = uint32(1)
 )
 
 var fetchCmd = &cobra.Command{
@@ -62,60 +68,83 @@ fetch http2 response from url: netool fetch https://www.google.com
 				log.Printf("* \tCertificate NotAfter: %v\n", cert.NotAfter)
 			}
 
-			if state.NegotiatedProtocol != "h2" {
+			if state.NegotiatedProtocol == "h2" {
+				log.Println("Successfully negotiated HTTP/2")
+
+				doneCh := make(chan struct{})
+				go func() {
+					defer close(doneCh)
+					readLoop(conn)
+				}()
+
+				//err = sendSettingsFrame(conn)
+				//if err != nil {
+				//	log.Printf("Failed to send SETTINGS frame: %v\n", err)
+				//	return
+				//}
+
+				// Send HTTP/2 request after successful upgrade
+				err = sendRequestHTTP2(conn, targetURL)
+				if err != nil {
+					log.Println("Failed to send HTTP/2 request:", err)
+					return
+				}
+
+				log.Printf("Send request done, url: %v\n", targetURL)
+				<-doneCh
+				return
+			} else if state.NegotiatedProtocol == "http/1.1" {
+				log.Println("Falling back to HTTP/1.1")
+			} else {
 				log.Println("Failed to negotiate HTTP/2, ALPN Negotiated Protocol:", state.NegotiatedProtocol)
 				return
 			}
-			log.Println("Successfully negotiated HTTP/2")
 		} else {
 			// Attempt to upgrade to HTTP/2 (h2c)
 			err = sendUpgradeRequestHTTP1(conn, targetURL)
 			if err != nil {
-				fmt.Println("Failed to send HTTP/1.1 Upgrade request:", err)
+				log.Println("Failed to send HTTP/1.1 Upgrade request:", err)
 				return
 			}
 
 			// Read the server's response to the upgrade request
 			upgraded, err := readUpgradeResponse(conn)
 			if err != nil {
-				fmt.Println("Failed to read upgrade response:", err)
+				log.Println("Failed to read upgrade response:", err)
 				return
 			}
 
 			if upgraded {
+				doneCh := make(chan struct{})
+				go func() {
+					defer close(doneCh)
+					readLoop(conn)
+				}()
+
 				// Send HTTP/2 request after successful upgrade
-				//err = sendRequestHTTP2(conn, rawURL)
-				//if err != nil {
-				//	fmt.Println("Failed to send HTTP/2 request:", err)
-				//	return
-				//}
-				//
-				//// Read frames from server
-				//readFrames(conn)
+				err = sendRequestHTTP2(conn, targetURL)
+				if err != nil {
+					log.Println("Failed to send HTTP/2 request:", err)
+					return
+				}
+
+				log.Printf("Send h2c request done, url: %v\n", targetURL)
+				<-doneCh
 				return
 			}
 		}
 
-		err = sendSettingsFrame(conn)
+		log.Println("start sendRequestHTTP1")
+		// Send HTTP/1.1 request if no upgrade
+		err = sendRequestHTTP1(conn, targetURL)
 		if err != nil {
-			log.Printf("Failed to send SETTINGS frame: %v\n", err)
+			fmt.Println("Failed to send HTTP/1.1 request:", err)
 			return
 		}
 
-		doneCh := make(chan struct{})
-		go func() {
-			defer close(doneCh)
-			readLoop(conn)
-		}()
-
-		err = sendRequest(conn, targetURL)
-		if err != nil {
-			fmt.Println("Failed to send request:", err)
-			return
-		}
-
-		log.Printf("Send request done, url: %v\n", targetURL)
-		<-doneCh
+		// Read HTTP/1.1 response
+		readResponseHTTP1(conn)
+		log.Println("finish readResponseHTTP1")
 	},
 }
 
@@ -123,42 +152,27 @@ func init() {
 	base.AddSubCommands(fetchCmd)
 }
 
-func dialConn(parsedURL *url.URL) (net.Conn, error) {
-	addr := getHostAddress(parsedURL)
-	if parsedURL.Scheme == "https" {
-		tlsConfig := &tls.Config{
-			InsecureSkipVerify: true, // NOTE: For testing only. Do not use in production.
-			NextProtos:         []string{"h2"},
-		}
-		conn, err := tls.Dial("tcp", addr, tlsConfig)
-		if err != nil {
-			log.Printf("dial conn err: %v\n", err)
-			return nil, err
-		}
-		return conn, nil
-	} else {
-		conn, err := net.Dial("tcp", addr)
-		if err != nil {
-			log.Printf("dial conn err: %v\n", err)
-			return nil, err
-		}
-		return conn, nil
-	}
-}
-
 func sendUpgradeRequestHTTP1(conn net.Conn, parsedURL *url.URL) error {
 	host := parsedURL.Host
 	path := parsedURL.RequestURI()
 
-	// Generate HTTP2-Settings header value (empty SETTINGS frame, base64 encoded)
-	settings := make([]byte, 0)
+	// Generate HTTP2-Settings header value with specific SETTINGS frame (base64 encoded)
+	settings := []byte{
+		0x00, 0x00, 0x0c, // Length (12 bytes)
+		0x04,                   // Type: SETTINGS (0x4)
+		0x00,                   // Flags
+		0x00, 0x00, 0x00, 0x00, // Stream ID: 0 (connection control frame)
+		// SETTINGS payload:
+		0x00, 0x03, 0x00, 0x00, 0x00, 0x64, // SETTINGS_MAX_CONCURRENT_STREAMS = 100
+		0x00, 0x04, 0x00, 0x00, 0x40, 0x00, // SETTINGS_INITIAL_WINDOW_SIZE = 16384
+	}
 	http2Settings := base64.StdEncoding.EncodeToString(settings)
 
 	// Create HTTP/1.1 Upgrade request
 	request := fmt.Sprintf(
 		"GET %s HTTP/1.1\r\n"+
 			"Host: %s\r\n"+
-			"User-Agent: golang-http1-client\r\n"+
+			"User-Agent: netool-fetch\r\n"+
 			"Accept: */*\r\n"+
 			"Connection: Upgrade, HTTP2-Settings\r\n"+
 			"Upgrade: h2c\r\n"+
@@ -191,7 +205,7 @@ func readUpgradeResponse(conn net.Conn) (bool, error) {
 		if er != nil {
 			return false, er
 		}
-		fmt.Print(line)
+		log.Print(line)
 		if strings.TrimSpace(line) == "" {
 			break
 		}
@@ -227,7 +241,7 @@ func sendSettingsFrame(conn net.Conn) error {
 	return nil
 }
 
-func sendRequest(conn net.Conn, parsedURL *url.URL) error {
+func sendRequestHTTP2(conn net.Conn, parsedURL *url.URL) error {
 	path := parsedURL.RequestURI()
 	host := parsedURL.Host
 
@@ -240,8 +254,6 @@ func sendRequest(conn net.Conn, parsedURL *url.URL) error {
 		{Name: "user-agent", Value: "netool-fetch"},
 		{Name: "accept", Value: "*/*"},
 	}
-
-	fmt.Printf("headers: %v\n", headers)
 
 	var headersBuffer bytes.Buffer
 	encoder := hpack.NewEncoder(&headersBuffer)
@@ -259,24 +271,93 @@ func sendRequest(conn net.Conn, parsedURL *url.URL) error {
 	frameHeader[0] = byte((length >> 16) & 0xFF) // Length (3 bytes)
 	frameHeader[1] = byte((length >> 8) & 0xFF)
 	frameHeader[2] = byte(length & 0xFF)
-	frameHeader[3] = 0x1                             // Type: HEADERS (0x1)
-	frameHeader[4] = 0x4                             // Flags: END_HEADERS (0x4)
-	binary.BigEndian.PutUint32(frameHeader[5:], 0x1) // Stream ID: 1
+	frameHeader[3] = 0x1                                                        // Type: HEADERS (0x1)
+	frameHeader[4] = 0x5                                                        // Flags: END_HEADERS | END_STREAM (0x5)
+	binary.BigEndian.PutUint32(frameHeader[5:], atomic.AddUint32(&streamID, 2)) // Stream ID (client-initiated, must be odd)
 
 	// Send the HEADERS frame
 	_, err := conn.Write(frameHeader)
 	if err != nil {
-		log.Printf("Send the HEADERS frame failure: %v\n", err)
 		return err
 	}
 	_, err = conn.Write(headersPayload)
 	if err != nil {
-		log.Printf("Send the HEADERS payload failure: %v\n", err)
 		return err
 	}
 
-	log.Println("Sent HTTP/2 request headers successful")
+	log.Printf("[HTTP/2] [%d] OPENED stream for %s\n", streamID, parsedURL)
+	log.Println("Sent HTTP/2 request headers")
+
 	return nil
+}
+
+func sendRequestHTTP1(conn net.Conn, parsedURL *url.URL) error {
+	host := parsedURL.Host
+	path := parsedURL.RequestURI()
+
+	// Create HTTP/1.1 request
+	request := fmt.Sprintf("GET %s HTTP/1.1\r\nHost: %s\r\ncontent-type: application/json; charset=utf-8\r\nUser-Agent: netool-fetch\r\nAccept: */*\r\nConnection: close\r\n\r\n", path, host)
+	log.Println(request)
+	_, err := conn.Write([]byte(request))
+	if err != nil {
+		return err
+	}
+	log.Println("Sent HTTP/1.1 request")
+	return nil
+}
+
+func readResponseHTTP1(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Println("Failed to read response:", err)
+			return
+		}
+		log.Print(line)
+		if strings.TrimSpace(line) == "" {
+			break
+		}
+	}
+
+	log.Println("Reading response body:")
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Println("Failed to read response body:", err)
+			return
+		}
+		log.Print(line)
+	}
+}
+
+func dialConn(parsedURL *url.URL) (net.Conn, error) {
+	addr := getHostAddress(parsedURL)
+	if parsedURL.Scheme == "https" {
+		tlsConfig := &tls.Config{
+			InsecureSkipVerify: true, // NOTE: For testing only. Do not use in production.
+			NextProtos:         []string{"h2", "http/1.1"},
+		}
+		conn, err := tls.Dial("tcp", addr, tlsConfig)
+		if err != nil {
+			log.Printf("dial conn err: %v\n", err)
+			return nil, err
+		}
+		return conn, nil
+	} else {
+		conn, err := net.Dial("tcp", addr)
+		if err != nil {
+			log.Printf("dial conn err: %v\n", err)
+			return nil, err
+		}
+		return conn, nil
+	}
 }
 
 func getHostAddress(parsedURL *url.URL) string {
