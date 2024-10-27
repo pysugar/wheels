feab4f3ca184b282100f2dd205905c08bbdd2a02
PySuger <zhanzizhi@gmail.com>
2024-10-27 12:49:30 +0800

    upgrade fetch

--- cmd/subcmds/fetch.go
+++ cmd/subcmds/fetch.go
@@ -2,9 +2,11 @@ package subcmds
 
 import (
 	"crypto/tls"
+	"fmt"
 	"github.com/pysugar/wheels/binproto/http2"
 	"log"
 	"net/url"
+	"strings"
 
 	"github.com/pysugar/wheels/cmd/base"
 	"github.com/spf13/cobra"
@@ -50,6 +52,12 @@ fetch http2 response from url: netool fetch https://www.google.com
 		}
 		log.Println("Successfully negotiated HTTP/2")
 
+		err = sendRequest(conn, targetURL)
+		if err != nil {
+			fmt.Println("Failed to send request:", err)
+			return
+		}
+
 		err = http2.ReadFrames(conn)
 		if err != nil {
 			log.Printf("read conn err: %v\n", err)
@@ -59,6 +67,35 @@ fetch http2 response from url: netool fetch https://www.google.com
 
 func init() {
 	base.AddSubCommands(fetchCmd)
+}
+
+func sendRequest(conn *tls.Conn, parsedURL *url.URL) error {
+	path := parsedURL.RequestURI()
+	host := parsedURL.Host
+
+	// Create HTTP/2 request headers
+	headers := []string{
+		fmt.Sprintf(":method: %s", "GET"),
+		fmt.Sprintf(":scheme: %s", parsedURL.Scheme),
+		fmt.Sprintf(":path: %s", path),
+		fmt.Sprintf(":authority: %s", host),
+		"user-agent: golang-http2-client",
+	}
+
+	// Encode headers as a simple format (not full HPACK encoding)
+	var headersBuffer strings.Builder
+	for _, header := range headers {
+		headersBuffer.WriteString(header)
+		headersBuffer.WriteString("\r\n")
+	}
+	headersBuffer.WriteString("\r\n")
+
+	// Send headers
+	if _, err := conn.Write([]byte(headersBuffer.String())); err != nil {
+		return err
+	}
+	fmt.Println("Sent HTTP/2 request headers")
+	return nil
 }
 
 func getHostAddress(parsedURL *url.URL) string {
